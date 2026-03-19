4ef6057feabbf782e5de303d2b67cd518c034023
MedyMa <75345465+MedyMa@users.noreply.github.com>
2026-03-19 15:30:57 +0800

    Update Passwall repository URLs in diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -18,8 +18,8 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 mkdir package/community
 pushd package/community
 git clone --depth=1 https://github.com/fw876/helloworld
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
